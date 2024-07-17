6fde5036ae431281d46f04c78ef4adffb5876058
Hagop Hovaguimian <hagop.microchip@gmail.com>
2024-07-18 03:10:41 +0400

    redisgned nav items

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { NavLinks } from "@/constants";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
 const Header = () => {
-  const pathName = usePathname();
 
   return (
     <section className="w-full h-20 bg-zinc-950">
@@ -24,26 +23,7 @@ const Header = () => {
           <nav className="w-fit h-[inherit]">
             <ul className="w-fit h-[inherit] flex items-center justify-start gap-x-6">
               {NavLinks.map((link, index) => (
-                <li key={index+1}>
-                  <Link href={link.url}>
-                    <div className={`relative py-1 px-6 rounded-xl hover:bg-zinc-800/90 ${link.url === pathName ? "bg-zinc-800/90" : ""}`}>
-                      <span className="w-fit h-fit flex items-center justify-center mx-auto">
-                        <Image
-                          src={link.icon}
-                          width={32}
-                          height={32}
-                          className={`object-contain duration-200 ${
-                            link.url === pathName
-                              ? "size-[38px] !fill-slate-500"
-                              : ""
-                          }`}
-                          alt=""
-                        />
-                      </span>
-                      <p className={`text-center ${link.url === pathName ? "font-extrabold" : ""}`}>{link.title}</p>
-                    </div>
-                  </Link>
-                </li>
+                <NavItem title={link.title} index={index} icon={link.icon} url={link.url}/>
               ))}
             </ul>
           </nav>
@@ -53,5 +33,33 @@ const Header = () => {
     </section>
   );
 };
+
+const NavItem = ({title, icon, url, index}: {title: string; icon: string; url: string; index: number;}) => {
+  const pathName = usePathname();
+  const [hover, setHover] = useState(false);
+
+  return (
+    <li key={index+1}>
+    <Link href={url}>
+      <div className={`relative py-1 px-6 rounded-xl`} onMouseOver={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+        <span className="w-fit h-fit flex items-center justify-center mx-auto">
+          <Image
+            src={icon}
+            width={32}
+            height={32}
+            className={`object-contain duration-200  ${
+              url === pathName
+                ? "size-[38px] !fill-slate-500 drop-shadow-[0_0_12px_#fff]"
+                : ""
+            } ${hover ? "drop-shadow-[0_0_12px_#fff]" : ""}`}
+            alt=""
+          />
+        </span>
+        <p className={`text-center ${url === pathName ? "font-extrabold" : ""}`}>{title}</p>
+      </div>
+    </Link>
+  </li>
+  )
+}
 
 export default Header;
