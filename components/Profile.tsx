a02c3e267a506ed4b2ca138f1ea6116b4c739cfd
Hagop Hovaguimian <hagop.microchip@gmail.com>
2024-07-19 13:21:42 +0400

    worked on profile design

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 import React from "react";
-import { Modal, ModalContent, ModalBody, ModalHeader } from "@nextui-org/react";
+import { Modal, ModalContent, ModalBody, ModalHeader, Button } from "@nextui-org/react";
 import { useSkin } from "@/hooks/useSkin";
 import Image from "next/image";
 import Loader from "./Loader";
+import { CgClose } from "react-icons/cg";
 
 export default function App({ isOpen, onOpenChange, name, region, tier, rank, setView }: any) {
 
@@ -17,22 +18,23 @@ export default function App({ isOpen, onOpenChange, name, region, tier, rank, se
   return (
     <>
       <Modal
+        closeButton={<button><CgClose size={32}/> </button>}
         isOpen={isOpen}
         onOpenChange={onOpenChange}
         classNames={{
-          body: "bg-stone-800 py-16 px-8",
+          body: "bg-neutral-900 py-16 px-8",
           backdrop: "bg-stone-900/20",
           base: "bg-inherit shadow-none border-none !text-white",
           closeButton:
-            "size-8 bg-stone-600 hover:bg-stone-500 text-stone-800 top-2 right-2 z-50",
+            "size-10 bg-stone-800 hover:bg-stone-500 text-stone-600 top-2 right-2 z-50 flex items-center justify-center",
           wrapper: "!items-center z-[9999999]",
-          header: "relative w-full h-24 bg-stone-400",
+          header: "relative w-full h-24 bg-stone-600",
         }}
       >
         <ModalContent>
           <ModalHeader>
             {/* FRAME */}
-            <div className="size-32 rounded-full bg-stone-900 absolute -bottom-12 left-5 overflow-clip flex items-center justify-center">
+            <div className="size-32 rounded-full bg-stone-950 absolute -bottom-12 left-5 overflow-clip flex items-center justify-center">
               {
                 skinURL ? (
                   <>
@@ -43,14 +45,14 @@ export default function App({ isOpen, onOpenChange, name, region, tier, rank, se
                 )
               }
             </div>
-            <strong className="font-bold tracking-wide text-2xl max-md:text-xl absolute -bottom-3 left-[146px] rounded-xl bg-stone-800 px-2">
+            <strong className="font-bold tracking-wide text-2xl max-md:text-xl absolute -bottom-3 left-[146px] rounded-xl bg-neutral-900 px-2">
               {name}
             </strong>
           </ModalHeader>
           <ModalBody>
             <div className="w-full h-fit">
               <span className="text-stone-500">Region:</span>
-              <div className="bg-neutral-900 w-48 h-9 rounded-xl flex items-center mt-2">
+              <div className="bg-neutral-950 w-48 h-9 rounded-xl flex items-center mt-2">
                 <div
                   className={`${
                     region.toLowerCase() === "eu"
@@ -96,7 +98,7 @@ export default function App({ isOpen, onOpenChange, name, region, tier, rank, se
 
             <div className="w-full h-fit">
               <span className="text-stone-500">Tier:</span>
-              <div className="bg-neutral-900 w-48 h-9 rounded-xl flex items-center mt-2">
+              <div className="bg-neutral-950 w-48 h-9 rounded-xl flex items-center mt-2">
                 <div
                   className={`text-black font-bold w-12 px-1 h-[inherit] rounded-tl-xl rounded-bl-xl flex items-center text-xl
                   ${
